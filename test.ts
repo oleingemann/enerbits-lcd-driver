34ee401d709dbd36d3986806e1802f79a7daee1f
Ole Ingemann Kjørmo <ole.kjormo@gmail.com>
2025-01-13 16:52:59 +0100

    minors

--- test.ts
+++ test.ts
@@ -1,47 +1,48 @@
-
-LCD1IN8.LCD_Init()
-LCD1IN8.LCD_SetBL(330)
-LCD1IN8.LCD_Clear()
-LCD1IN8.LCD_ClearBuf()
-LCD1IN8.DrawPoint(
-41,
-18,
-14270,
-DOT_PIXEL.DOT_PIXEL_4
-)
-LCD1IN8.DrawLine(
-0,
-128,
-160,
-1,
-14798,
-DOT_PIXEL.DOT_PIXEL_2,
-LINE_STYLE.LINE_DOTTED
-)
-LCD1IN8.DrawRectangle(
-21,
-42,
-56,
-67,
-29596,
-DRAW_FILL.DRAW_EMPTY,
-DOT_PIXEL.DOT_PIXEL_1
-)
-LCD1IN8.DrawCircle(
-70,
-55,
-9,
-19555,
-DRAW_FILL.DRAW_EMPTY,
-DOT_PIXEL.DOT_PIXEL_1
-)
-LCD1IN8.DisString(
-56,
-38,
-"hello",
-28011
-)
-LCD1IN8.LCD_Display()
-basic.forever(function () {
-	
-})
+namespace test {
+    LCD1IN8.LCD_Init()
+    LCD1IN8.LCD_SetBL(330)
+    LCD1IN8.LCD_Clear()
+    LCD1IN8.LCD_ClearBuf()
+    LCD1IN8.DrawPoint(
+        41,
+        18,
+        14270,
+        LCD1IN8.DOT_PIXEL.DOT_PIXEL_4
+    )
+    LCD1IN8.DrawLine(
+        0,
+        128,
+        160,
+        1,
+        14798,
+        LCD1IN8.DOT_PIXEL.DOT_PIXEL_2,
+        LCD1IN8.LINE_STYLE.LINE_DOTTED
+    )
+    LCD1IN8.DrawRectangle(
+        21,
+        42,
+        56,
+        67,
+        29596,
+        LCD1IN8.DRAW_FILL.DRAW_EMPTY,
+        LCD1IN8.DOT_PIXEL.DOT_PIXEL_1
+    )
+    LCD1IN8.DrawCircle(
+        70,
+        55,
+        9,
+        19555,
+        LCD1IN8.DRAW_FILL.DRAW_EMPTY,
+        LCD1IN8.DOT_PIXEL.DOT_PIXEL_1
+    )
+    LCD1IN8.DisString(
+        56,
+        38,
+        "hello",
+        28011
+    )
+    LCD1IN8.LCD_Display()
+    basic.forever(function () {
+        
+    })
+}
